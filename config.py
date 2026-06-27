5d27b83ecd62d097e69d72cdc368914f3844898f
SowinskiBraeden <braeden@sowinski.dev>
2026-06-26 21:54:37 -0700

    chore: bump version to 2.5.3

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DATA_PATH = BASE_DIR / "data" / "entries.csv"
 DEFAULT_DATABASE_URL = f"sqlite:///{BASE_DIR / 'data' / 'boker-dev.sqlite3'}"
 
 ELIGIBLE_MIN_SESSIONS = 3
-APP_VERSION = "2.5.2"
+APP_VERSION = "2.5.3"
 
 
 def load_local_env(env_path: Path) -> None:
